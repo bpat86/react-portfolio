c694483da5c86dc75e821577b620f3b1fb575926
bpat86 <b.patterson0486@gmail.com>
2018-05-04 16:29:30 -0700

    Fixed a typo on the About page

--- src/scripts/components/AboutMe.js
+++ src/scripts/components/AboutMe.js
@@ -15,7 +15,7 @@ export default class About extends Component {
                         <div className="lrg-text-size bold mb-1">About Me</div>
                         <div className="line-seperator mb-1 bg-white faded"></div>
                         <p className="intro lh-normal mb-2">I try not to take myself too seriously. I also try to maintain a healthy work/life balance, though it can be pretty difficult sometimes to not get completely <span className="strikeThrough">wrapped up in the shame spiral that is scrambling to learn the latest framework or some other new thing that every other developer has moved on to so I don't feel left behind</span> wrapped up in fun side projects. What can I say really, it's a labor of love.</p>
-                        <p className="intro lh-normal mb-2">When not reclined in front of my laptop and getting minimal hours of sleep, I enjoy spending time with my friends & family, <span className="strikeThrough">using all my PTO days</span> traveling, playing my guitars (poorly), weight lifting, engaging in the relentless pursuit of knowledge and happiness.</p>
+                        <p className="intro lh-normal mb-2">When not reclined in front of my laptop and getting minimal hours of sleep, I enjoy spending time with my friends & family, <span className="strikeThrough">using all my PTO days</span> traveling, playing my guitars (poorly), weight lifting, and engaging in the relentless pursuit of knowledge and happiness.</p>
                         <div className="lrg-text-size bold mb-1">Get in Touch</div>
                         <div className="line-seperator mb-1 bg-white faded"></div>
                         <p className="intro lh-normal mb-2">
