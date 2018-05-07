360be732996af977b13366c978d5b43b5a1f8372
bpat86 <b.patterson0486@gmail.com>
2018-05-07 13:56:47 -0700

    Updated robots.txt

--- src/scripts/components/Email.js
+++ src/scripts/components/Email.js
@@ -7,8 +7,6 @@ export default class Email extends Component {
             <span>
                 Send me an email at <a className="paragraph-link bg-round" href="mailto:&#098;&#111;&#098;&#098;&#121;&#112;&#097;&#116;&#116;&#101;&#114;&#115;&#111;&#110;&#052;&#048;&#052;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">
                 &#098;&#111;&#098;&#098;&#121;&#112;&#097;&#116;&#116;&#101;&#114;&#115;&#111;&#110;&#052;&#048;&#052;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;<LinkArrow /></a>
-                <input id="name" name="name" type="hidden" />
-                <input id="message" name="message" type="hidden" />
             </span>
         );
     }
