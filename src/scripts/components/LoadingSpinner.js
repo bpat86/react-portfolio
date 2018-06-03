b9f3e4bff940a636a4e502ab96fb55f0819300dc
bpat86 <b.patterson0486@gmail.com>
2018-06-03 10:27:16 -0700

    Added silly text to the LoadingSpinner

--- src/scripts/components/LoadingSpinner.js
+++ src/scripts/components/LoadingSpinner.js
@@ -5,9 +5,10 @@ export default class LoadingSpinner extends Component {
 	render() {
 		return (
 			<div className="loading-container vh-100">
-				<div className="loading flex-align-center mx-auto user-select">
+				<div className="loading flex-align-center flex-column mx-auto user-select">
 					<img alt="Loading..." src={Loader} />
 					<div className="text">Loading...</div>
+					<div className="sm-text-size fw-semi-bold text-light-gray mt-05">It's going to Space! Can you give it a second to get back from Space?</div>
 				</div>
 			</div>
 	    );
